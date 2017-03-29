2edddec142984cb43c5fcccdebc735a2667c1d00
ChiharuTerashima <chichi.pa2.1091@gmail.com>
2017-03-29 22:31:44 +0900

    新規作成

---
+++ charServer.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from bottle import get, template, run
+from bottle.ext.websocket import GeventWebSocketServer
+from bottle.ext.websocket import websocket
+
+users = set()
+
+@get('/')
+def index():
+    return template('views/index')
+
+@get('/websocket', apply=[websocket])
+def chat(ws):
+    users.add(ws)
+    while True:
+        msg = ws.receive()
+        if msg is not None:
+            for u in users:
+                u.send(msg)
+        else:
+            break
+    users.remove(ws)
+
+run(host='127.0.0.1', port=8080, server=GeventWebSocketServer)
